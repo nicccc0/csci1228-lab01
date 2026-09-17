54a4cba40fe19e71f55f36fa4863c92e3e46e4c3
Nico Sharpe <Nico.Sharpe@smu.ca>
2026-09-16 23:15:13 -0300

    Predictions before running the programs

---
+++ src/Trace1.java
@@ -0,0 +1,12 @@
+public class Trace1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+
+        System.out.println(a / b);
+        System.out.println(a / (double) b);
+        System.out.println((double) (a / b));
+        System.out.println(1 / 2 + 0.5);
+        System.out.println(0.1 + 0.2);
+    }
+}
